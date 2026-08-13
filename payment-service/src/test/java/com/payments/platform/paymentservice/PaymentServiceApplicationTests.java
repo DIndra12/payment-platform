57d611fd878048e97315758a8d1809171170d310
Indranil Das <ddas.indranil@gmail.com>
2026-08-13 23:48:33 +0530

    Test Files added

--- payment-service/src/test/java/com/payments/platform/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/payments/platform/paymentservice/PaymentServiceApplicationTests.java
@@ -2,12 +2,30 @@ package com.payments.platform.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class PaymentServiceApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16-alpine");
+
+    @Container
+    @ServiceConnection
+    static KafkaContainer kafka =
+            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
 
     @Test
     void contextLoads() {
     }
-
 }
