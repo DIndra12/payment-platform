9f3c68319991664b6e65cdf2e0e77134455621dd
Indranil Das <ddas.indranil@gmail.com>
2026-08-15 08:10:18 +0530

    Test Files added

--- account-service/src/main/java/com/payments/platform/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/payments/platform/accountservice/AccountServiceApplication.java
@@ -2,8 +2,12 @@ package com.payments.platform.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.payments.platform.accountservice.repository")
+@EntityScan("com.payments.platform.accountservice.entity")
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
